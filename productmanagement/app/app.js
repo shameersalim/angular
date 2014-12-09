fd1720feece36cc3e8944f8bd5033f662262037f
touchydeer36 <shameersalim@outlook.com>
2014-12-09 15:58:25 -0500

    Updated code

--- productmanagement/app/app.js
+++ productmanagement/app/app.js
@@ -4,5 +4,40 @@
 (function () {
     "use strict";
     var app = angular.module("productManagement",
-        ["common.services", "ProductResourceMock"]);
+        ["ui.router","common.services",
+            "ProductResourceMock"
+            ]);
+
+
+    app.config(
+        ["$stateProvider",
+            "$urlRouterProvider",
+            function ($stateProvider, $urlRouterProvider) {
+                $urlRouterProvider.otherwise("/");
+                $stateProvider
+
+                    .state("home", {
+                        url: "/",
+                        templateUrl: "app/WelcomeView.html",
+                    })
+
+                    .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/ProductListView.html",
+                    controller: "ProductListController as vm"
+                    })
+
+                 .state("productEdit", {
+                     url: "/products/edit/:productId",
+                     templateUrl: "app/products/ProductEditView.html",
+                     controller: "ProductEditController as vm"
+                 })
+
+                .state("productDetail", {
+                    url:"/products/:productId",
+                    templateUrl: "app/products/ProductDetailView.html",
+                    controller: "ProductDetailController as vm"
+                })
+
+            }])
 }());
---
+++ productmanagement/app/products/ProductDetailController.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    "use strict";
+
+    angular.module("productManagement")
+    .controller("ProductDetailController", ProductDetailController_init);
+
+
+    function ProductDetailController_init() {
+        var vm = this;
+
+        // vm.product = product;
+        vm.product = {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2009",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "cost": 9.00,
+            "price": 19.95,
+            "category": "garden",
+            "tags": ["leaf", "tool"],
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        };
+
+        vm.title = "Product Detail: " + vm.product.productName;
+
+        if (vm.product.tags) {
+            vm.product.tagList = vm.product.tags.toString();
+        }
+    }
+}());
