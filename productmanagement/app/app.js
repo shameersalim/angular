bc79b045c2edef8a1e5014d5f6534189eb164eff
touchydeer36 <shameersalim@outlook.com>
2014-12-09 12:32:13 -0500

    Angular Line of business

---
+++ productmanagement/app/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Deb on 8/20/2014.
+ */
+(function () {
+    "use strict";
+    var app = angular.module("productManagement",
+        ["common.services", "ProductResourceMock"]);
+}());
---
+++ productmanagement/app/common/services/ProductResource.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("productResource",
+        ["$resource",
+            productResource
+        ]);
+
+    function productResource($resource) {
+        return $resource("/api/products/:productId");
+    }
+
+
+}());
